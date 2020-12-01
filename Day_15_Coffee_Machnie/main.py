8e552b82ab98acacf32eaf98249582de88c93bec
syt1209 <syt1209@gmail.com>
2020-12-01 13:04:31 -0500

    Day 15-Coffee machine project start

---
+++ Day_15_Coffee_Machnie/main.py
@@ -0,0 +1,45 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino): "
+
+# TODO 2. Turn off the coffee machine by entering "off" to the prompt
+
+# TODO 3. Print report. When user enters "report", generate current Resource values and Money earned
+
+# TODO 4. Check resources sufficient? Check and feedback to user
+
+# TODO 5. Process coins.
+
+# TODO 6. Check transaction successful?
+
+# TODO 7. Make coffee.
